32fa5605fbe2c8fe9df83867481291e126832be1
gfons99 <igadu_99@outlook.com>
2024-10-18 20:31:12 -0600

    ejercicio02_1 led on-off, creado

--- practica05.sh
+++ practica05.sh
@@ -1,6 +1,5 @@
-
+root@raspberrypi:/home/gfons# raspi-config
 # Seleccionamos las siguientes opciones
-
 -> 3 Interface Options Configure connections to peripherals
 ...
 -> I5  I2C Enable/disable automatic loading of I2C kernel module
@@ -11,14 +10,12 @@ Would you like the ARM I2C interface to be enabled?
 The ARM I2C interface is enabled
 -> <Ok>
 
-######## changed
 root@raspberrypi:/home/gfons# apt-get install i2c-tools python3-smbus
 ...
 root@raspberrypi:/home/gfons# adduser gfons i2c
 Adding user `gfons' to group `i2c' ...
 Done.
 
-######## SAME
 gfons@raspberrypi:~ $ python3 -m venv .venv
 gfons@raspberrypi:~ $ source .venv/bin/activate
 (.venv) gfons@raspberrypi:~ $ pip3 install smbus2
