e23197f912f1b4378af4cd359145a05cf81f0849
gfons99 <igadu_99@outlook.com>
2024-11-14 17:15:49 -0600

    código simplificado, se agrega control de bomba y luz

--- proyecto/menutelegram.py
+++ proyecto/menutelegram.py
@@ -6,37 +6,38 @@ import struct
 import time
 import threading
 import RPi.GPIO as GPIO
-from gpiozero import PWMLED  # Import the PWMLED class for controlling the LED brightness
 
 SLAVE_ADDR = 0x08  # I2C address for the Arduino
 
 # Initialize the I2C bus
 i2c = smbus2.SMBus(1)
 GPIO.setmode(GPIO.BCM)
-# Initialize a PWM LED on GPIO 4
-led = PWMLED(4)
 
-# Configuración de los pines GPIO
+# Configuración de los pines
+## Motor 1
 IN1_1= 14  # Pin GPIO conectado a IN1 del ULN2003
 IN2_1= 15  # Pin GPIO conectado a IN2 del ULN2003
 IN3_1= 18  # Pin GPIO conectado a IN3 del ULN2003
 IN4_1= 23  # Pin GPIO conectado a IN4 del ULN2003
-# Configuración de los pines GPIO para el motor 2 motor izquierdo
-IN1_2 = 25  # Pin GPIO conectado a IN1 del ULN2003 para el motor 2
-IN2_2 = 8   # Pin GPIO conectado a IN2 del ULN2003 para el motor 2
-IN3_2 = 7   # Pin GPIO conectado a IN3 del ULN2003 para el motor 2
-IN4_2 = 1   # Pin GPIO conectado a IN4 del ULN2003 para el motor 2
-# Configuración de los pines
-
-
 GPIO.setup(IN1_1, GPIO.OUT)
 GPIO.setup(IN2_1, GPIO.OUT)
 GPIO.setup(IN3_1, GPIO.OUT)
 GPIO.setup(IN4_1, GPIO.OUT)
+## Motor 2
+IN1_2 = 25  # Pin GPIO conectado a IN1 del ULN2003 para el motor 2
+IN2_2 = 8   # Pin GPIO conectado a IN2 del ULN2003 para el motor 2
+IN3_2 = 7   # Pin GPIO conectado a IN3 del ULN2003 para el motor 2
+IN4_2 = 1   # Pin GPIO conectado a IN4 del ULN2003 para el motor 2
 GPIO.setup(IN1_2, GPIO.OUT)
 GPIO.setup(IN2_2, GPIO.OUT)
 GPIO.setup(IN3_2, GPIO.OUT)
 GPIO.setup(IN4_2, GPIO.OUT)
+# Acopladores
+pin_bomba   = 17
+pin_lampara = 27
+GPIO.setup(pin_bomba, GPIO.OUT)
+GPIO.setup(pin_lampara, GPIO.OUT)
+
 # Definición de la secuencia para el motor
 step_sequence = [
     [1, 0, 0, 1],
@@ -79,15 +80,20 @@ TOKEN = '7464400664:AAH6KvXIJnE3j-ft8jC8nVEf7BrXVIVkaM0'
 async def iniciar(update: Update, context):
     # Crea el menú inicial
     teclado = [
-        [InlineKeyboardButton("Reporte general (reporte)", callback_data='sensor_1')],
-        [InlineKeyboardButton("Solución nutritiva hidropónica", callback_data='sensor_2')],
-        [InlineKeyboardButton("Temperatura", callback_data='sensor_3')],
-        [InlineKeyboardButton("Humedad", callback_data='sensor_4')],
-        [InlineKeyboardButton("Iluminacion recibida", callback_data='sensor_5')],
-        [InlineKeyboardButton("PH", callback_data='sensor_6_')],
-        [InlineKeyboardButton("Turbidez", callback_data='sensor_7')],
-        [InlineKeyboardButton("Abrir Compuertas", callback_data='sensor_8')],
-        [InlineKeyboardButton("Apagado de luz", callback_data='sensor_9')],
+        [InlineKeyboardButton("Reporte general (reporte)", callback_data='reporte')],
+        # Sensores adc
+        [InlineKeyboardButton("Solución nutritiva hidropónica", callback_data='sensor_conductividad')],
+        [InlineKeyboardButton("Turbidez", callback_data='sensor_turbidez')],
+        [InlineKeyboardButton("Temperatura", callback_data='sensor_temperatura')],
+        [InlineKeyboardButton("PH", callback_data='sensor_ph')],
+        [InlineKeyboardButton("Iluminacion recibida", callback_data='sensor_luminosidad')],
+        # Sensores extra
+        [InlineKeyboardButton("Humedad", callback_data='sensor_humedad')],
+        [InlineKeyboardButton("Abrir Compuertas", callback_data='compuertas')],
+        [InlineKeyboardButton("Encender bomba", callback_data='sensor_bomba_on')],
+        [InlineKeyboardButton("Apagar bomba", callback_data='sensor_bomba_off')],
+        [InlineKeyboardButton("Encender luz", callback_data='sensor_luz_on')],
+        [InlineKeyboardButton("Apagar luz", callback_data='sensor_luz_off')],
     ]
     reply_markup = InlineKeyboardMarkup(teclado)
     await update.message.reply_text('Bienvenido, selecciona la opción de tu preferencia:', reply_markup=reply_markup)
@@ -102,162 +108,69 @@ async def boton(update: Update, context):
     await asyncio.sleep(3)
 
     # Procesar según la opción seleccionada
-    if consulta.data == 'sensor_1':
-        await procesar_sensor_1(consulta)
-    elif consulta.data == 'sensor_2':
-        await procesar_sensor_2(consulta)
-    elif consulta.data == 'sensor_3':
-        await procesar_sensor_3(consulta)
-    elif consulta.data == 'sensor_4':
-        await procesar_sensor_4(consulta)
-    elif consulta.data == 'sensor_5':
-        await procesar_sensor_5(consulta)
-    elif consulta.data == 'sensor_6':
-        await procesar_sensor_6(consulta)
-    elif consulta.data == 'sensor_7':
-        await procesar_sensor_7(consulta)
-    elif consulta.data == 'sensor_8':
-        await procesar_sensor_8(consulta)
-    elif consulta.data == 'sensor_9':
-        await procesar_sensor_9(consulta)
+    if consulta.data == 'reporte':
+        await procesar_reporte(consulta)
+
+    elif consulta.data == 'sensor_conductividad':
+        await procesar_un_sensor(consulta, 0, "conductividad", )
+    elif consulta.data == 'sensor_turbidez':
+        await procesar_un_sensor(consulta, 1, "turbidez")
+    elif consulta.data == 'sensor_temperatura':
+        await procesar_un_sensor(consulta, 2, "temperatura")
+    elif consulta.data == 'sensor_ph':
+        await procesar_un_sensor(consulta, 3, "ph")
+    elif consulta.data == 'sensor_luminosidad':
+        await procesar_un_sensor(consulta, 4, "luminosidad")
+
+    # elif consulta.data == 'sensor_humedad':
+    #     await procesar_un_sensor(consulta, 0, "humedad")
+
+    elif consulta.data == 'sensor_bomba_on':
+        GPIO.output(pin_bomba, GPIO.HIGH)
+    elif consulta.data == 'sensor_bomba_off':
+        GPIO.output(pin_bomba, GPIO.LOW)
+    elif consulta.data == 'sensor_luz_on':
+        GPIO.output(pin_lampara, GPIO.HIGH)
+    elif consulta.data == 'sensor_luz_off':
+        GPIO.output(pin_lampara, GPIO.LOW)
+
+    elif consulta.data == 'compuertas':
+        await procesar_compuertas(consulta)
 
     # Mostrar el menú principal después de procesar
     await mostrar_menu(consulta, context)
 
 
-async def procesar_sensor_1(consulta):
+async def procesar_un_sensor(consulta, index, parametro):
+    try:
+        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
+        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
+        i2c.i2c_rdwr(msg)
+        data = list(msg)
+        ba = bytearray(data)
+        analog_values = struct.unpack('<fffff', ba)
+        # Mostrar el valor medido por el sensor
+        await consulta.edit_message_text(text=f"{parametro}: {analog_values[index]:.2f} V")
+    except Exception as e:
+        await consulta.edit_message_text(text=f"Error al leer el sensor de {parametro}: {str(e)}")
+
+
+# Reporte general
+async def procesar_reporte(consulta):
     await consulta.edit_message_text(text="Procesando... Reporte.")
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"Conductividad: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de Conductividad: {str(e)}")
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"Temperatura: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de temperatura: {str(e)}")
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"Iluminacion recibida: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de Iluminacion: {str(e)}")
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"Turbidez: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de Turbidez: {str(e)}")
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"PH: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de PH: {str(e)}")
 
-
-async def procesar_sensor_2(consulta):
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"Conductividad: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de Conductividad: {str(e)}")
-
-async def procesar_sensor_3(consulta):
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"Temperatura: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de Temperatura: {str(e)}")
+    await procesar_un_sensor("conductividad", consulta)
+    await procesar_un_sensor("turbidez", consulta)
+    await procesar_un_sensor("temperatura", consulta)
+    await procesar_un_sensor("ph", consulta)
+    await procesar_un_sensor("luminosidad", consulta)
 
 
 async def procesar_sensor_4(consulta):
     await consulta.edit_message_text(text="Procesando..Humedad")
 
 
-async def procesar_sensor_5(consulta):
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"Iluminacion recibida: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de Iluminacion: {str(e)}")
-
-
-async def procesar_sensor_6(consulta):
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"PH: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de PH: {str(e)}")
-
-
-async def procesar_sensor_7(consulta):
-    try:
-        # Simular la lectura de valores de un sensor (esto debería ser reemplazado por la lectura real del sensor)
-        msg = smbus2.i2c_msg.read(SLAVE_ADDR, 20)
-        i2c.i2c_rdwr(msg)
-        data = list(msg)
-        ba = bytearray(data)
-        analog_values = struct.unpack('<fffff', ba)
-        # Mostrar el primer valor (por ejemplo, la conductividad)
-        await consulta.edit_message_text(text=f"Turbidez: {analog_values[0]:.2f} V")
-    except Exception as e:
-        await consulta.edit_message_text(text=f"Error al leer el sensor de Turbidez: {str(e)}")
-
-
-async def procesar_sensor_8(consulta):
+async def procesar_compuertas(consulta):
     def rotate_motor1_to_initial_position():
         steps_per_rev = 507  # Pasos para una vuelta completa
         steps_to_move = int(90 / (360 / steps_per_rev))  # Pasos para 90 grados
@@ -295,21 +208,27 @@ async def procesar_sensor_8(consulta):
 
     finally:
         GPIO.cleanup()
-async def procesar_sensor_9(consulta):
+async def procesar_bomba_agua(consulta):
     await consulta.edit_message_text(text="LUZ")
+
 
 async def mostrar_menu(consulta, context):
     # Crea el menú principal de nuevo después de cada consulta
     teclado = [
-        [InlineKeyboardButton("Reporte general (reporte)", callback_data='sensor_1')],
-        [InlineKeyboardButton("Solución nutritiva hidropónica", callback_data='sensor_2')],
-        [InlineKeyboardButton("Temperatura", callback_data='sensor_3')],
-        [InlineKeyboardButton("Humedad", callback_data='sensor_4')],
-        [InlineKeyboardButton("Iluminacion recibida", callback_data='sensor_5')],
-        [InlineKeyboardButton("PH", callback_data='sensor_6_')],
-        [InlineKeyboardButton("Turbidez", callback_data='sensor_7')],
-        [InlineKeyboardButton("Abrir Compuertas", callback_data='sensor_8')],
-        [InlineKeyboardButton("Apagado de luz", callback_data='sensor_9')],
+        [InlineKeyboardButton("Reporte general (reporte)", callback_data='reporte')],
+        # Sensores adc
+        [InlineKeyboardButton("Solución nutritiva hidropónica", callback_data='sensor_conductividad')],
+        [InlineKeyboardButton("Turbidez", callback_data='sensor_turbidez')],
+        [InlineKeyboardButton("Temperatura", callback_data='sensor_temperatura')],
+        [InlineKeyboardButton("PH", callback_data='sensor_ph')],
+        [InlineKeyboardButton("Iluminacion recibida", callback_data='sensor_luminosidad')],
+        # Sensores extra
+        [InlineKeyboardButton("Humedad", callback_data='sensor_humedad')],
+        [InlineKeyboardButton("Abrir Compuertas", callback_data='compuertas')],
+        [InlineKeyboardButton("Encender bomba", callback_data='sensor_bomba_on')],
+        [InlineKeyboardButton("Apagar bomba", callback_data='sensor_bomba_off')],
+        [InlineKeyboardButton("Encender luz", callback_data='sensor_luz_on')],
+        [InlineKeyboardButton("Apagar luz", callback_data='sensor_luz_off')],
     ]
 
     reply_markup = InlineKeyboardMarkup(teclado)
@@ -331,4 +250,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
